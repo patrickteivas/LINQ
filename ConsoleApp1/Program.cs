ee80bf730bd031c6f38b527f903b4253286cf5a3
patrickteivas <teivas.2011@mail.ru>
2018-02-22 09:52:03 +0200

    Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string Path = System.IO.Directory.GetCurrentDirectory();
+            string Text = System.IO.File.ReadAllText(Path + "/../../../tekst.txt");
+            var CharactersCount = Text.ToUpper().Where(Char.IsLetter).GroupBy(x => x).Select(x => new { Char = x.Key, Count = x.Count() });
+            var CharactersCountDescending = (from element in CharactersCount
+                                            orderby element.Count descending
+                                            select element).ToList();
+
+            Console.WriteLine("1) Sõnu tekstis: " + Text.Split(' ').ToList().Count);
+            Console.WriteLine("2) Erinevat tähte tekstis: " + Text.ToLower().Where(Char.IsLetter).Distinct().Count());
+            Console.WriteLine("3) Mitu korda esineb erinevat tähte tekstis:");
+            foreach (var item in CharactersCountDescending)
+            {
+                Console.WriteLine(item.Char + " " + item.Count);
+            }
         }
     }
 }
